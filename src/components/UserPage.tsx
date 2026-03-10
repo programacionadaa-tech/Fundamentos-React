e7c8a4dc9a7e0b393e2d2713f2502f48a10068f7
programacionadaa-tech <programacionadaa@gmail.com>
2026-03-10 12:43:05 -0600

    userPage

--- src/components/UserPage.tsx
+++ src/components/UserPage.tsx
@@ -1,12 +1,31 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
+import axios from "axios";
+
+interface User {
+    id: number;
+    email: string;
+    first_name: string;
+    last_name: string;
+    avatar: string;
+}
 
 export const UserPage = () => {
     
-    useEffect(() => {
-        fetch('https://reqres.in/api/users?page=2')
+    const [users, setUsers] = useState<User[]>([]);
+
+    axios.get('https://reqres.in/api/users?page=2')
+        .then(resp => setUsers(resp.data.data))
+
+
+    /*useEffect(() => {
+        fetch('https://reqres.in/api/users?page=2', {
+            headers: {
+                'x-api-key': 'pub_44baf7989c10e6e8f149fecb47b6535e7d9c2a4358e1a81c3a41bdb7e7ff2d81'
+            }
+        })
             .then(response => response.json())
-            .then(data => console.log(data))
-    }, [])
+            .then(data => setUsers(data.data))
+    }, [])*/
     
     return (
         <>
@@ -20,11 +39,13 @@ export const UserPage = () => {
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>Avatar</td>
-                        <td>Nombre</td>
-                        <td>Email</td>
-                    </tr>
+                    {users.map(user => (
+                        <tr key={user.id}>
+                            <td><img src={user.avatar} alt={user.first_name} width={50} /></td>
+                            <td>{user.first_name} {user.last_name}</td>
+                            <td>{user.email}</td>
+                        </tr>
+                    ))}
                 </tbody>
             </table>
         </>
